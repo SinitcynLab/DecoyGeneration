180ecc4c770da4cdec8911721d5f4450ff2b3f58
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-11-13 10:38:36 +0100

    Removed GPU request

--- src/bash/run_job.sh
+++ src/bash/run_job.sh
@@ -2,13 +2,12 @@
 
 #SBATCH --job-name=my_job
 #SBATCH --output=my_job_output_%j.txt
-#SBATCH --partition=mcs.gpu.q         # Choose a partition that has GPUs
+#SBATCH --partition=mcs.default.q         # Choose a partition that has GPUs
 #SBATCH --time=16:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=64G
-#SBATCH --gpus=1                      # This is how to request a GPU
+#SBATCH --mem-per-cpu=64G            # This is how to request a GPU
 
 module load PyTorch/2.1.2-foss-2023a
 
