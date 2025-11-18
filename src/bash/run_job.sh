76cc26bea2f3216ba3eeb23ee78971f60f18937c
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-11-18 11:58:24 +0100

    Update run_job.sh

--- src/bash/run_job.sh
+++ src/bash/run_job.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 #SBATCH --job-name=my_job
-#SBATCH --output=my_job_output_%j.txt
-#SBATCH --partition=mcs.default.q         # Choose a partition that has GPUs
-#SBATCH --time=4:00:00
+#SBATCH --output=cv_%j.txt
+#SBATCH --partition=tue.gpu.q         # Choose a partition that has GPUs
+#SBATCH --time=16:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=64G
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=2G
+#SBATCH --gpus=1                      # This is how to request a GPU
 
 module load PyTorch/2.1.2-foss-2023a
 
-python src/main.py
+python src/test/cross_val_test.py
