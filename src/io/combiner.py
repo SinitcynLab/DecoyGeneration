d09200a9e148e6c9fdc9261ecf6aed52a3b99933
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-11-23 12:06:29 +0100

    Implemented combiner and relabler

---
+++ src/io/combiner.py
@@ -0,0 +1,14 @@
+
+from typing import Iterable, List
+from src.io.fasta import FastaRecord, read_fasta_file, write_fasta_file
+
+class Combiner(object):
+    def __init__(self):
+        object.__init__(self)
+
+    def combine(in_file_names: Iterable[str], out_file_name: str):
+        all_records: List[FastaRecord] = []
+        for file_name in in_file_names:
+            file_records = read_fasta_file(file_name)
+            all_records += file_records
+        write_fasta_file(all_records, out_file_name)
---
+++ src/io/relabler.py
@@ -0,0 +1,15 @@
+
+from typing import List
+from src.io.fasta import FastaRecord, read_fasta_file, write_fasta_file
+
+class Combiner(object):
+    def __init__(self):
+        object.__init__(self)
+
+    def relable(label:str, in_file_name: str, out_file_name: str):
+        in_records: List[FastaRecord] = read_fasta_file(in_file_name)
+        out_records: List[FastaRecord] = []
+        for in_record in in_records:
+            new_head = label + in_record.head
+            out_records.append(FastaRecord(new_head, in_record.sequence))
+        write_fasta_file(out_records, out_file_name)
