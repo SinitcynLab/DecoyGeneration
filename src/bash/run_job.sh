3dbd403a7eedd5badd4d8bad61954b0a9f3e861f
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-11-23 22:41:04 +0100

    Changed job script

--- src/bash/run_job.sh
+++ src/bash/run_job.sh
@@ -3,11 +3,11 @@
 #SBATCH --job-name=my_job
 #SBATCH --output=gen_%j.txt
 #SBATCH --partition=tue.gpu.q         # Choose a partition that has GPUs
-#SBATCH --time=24:00:00
+#SBATCH --time=10:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=32G
+#SBATCH --mem-per-cpu=40G
 #SBATCH --gpus=1
 
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
