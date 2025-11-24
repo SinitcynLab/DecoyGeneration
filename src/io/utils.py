350abb69b7e3de5df8e06ddc620cbb32f55f973d
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-11-24 01:55:08 +0100

    updated job

--- src/io/utils.py
+++ src/io/utils.py
@@ -1,7 +1,7 @@
 from typing import List
 
 def remove_long_sequences(sequences: List[str], cap_length: int = 10_000) -> List[str]:
-    for i in range(len(sequences)):
+    for i, _ in enumerate(sequences):
         if len(sequences[i]) > cap_length:
             sequences.pop(i)
     return sequences
