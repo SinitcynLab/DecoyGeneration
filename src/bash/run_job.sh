26244081d44ae75d661926d10d66cc4c47f24ded
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-11-25 17:26:09 +0100

    Update run_job.sh

--- src/bash/run_job.sh
+++ src/bash/run_job.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #SBATCH --job-name=my_job
-#SBATCH --output=cv_%j.txt
-#SBATCH --partition=tue.gpu.q         # Choose a partition that has GPUs
+#SBATCH --output=cnn_test_%j.txt
+#SBATCH --partition=mcs.gpu.q         # Choose a partition that has GPUs
 #SBATCH --time=16:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=32G
+#SBATCH --mem-per-cpu=16G
 #SBATCH --gpus=1
 
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 
-python -u src/run/cross_val_rnn.py
+python -u src/run/cnn_test.py
