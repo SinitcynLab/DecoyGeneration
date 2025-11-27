7e6646b00a396c966b2e503b221200fac8cdb0a0
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-11-27 01:32:17 +0100

    Update run_job.sh

--- src/bash/run_job.sh
+++ src/bash/run_job.sh
@@ -3,11 +3,11 @@
 #SBATCH --job-name=my_job
 #SBATCH --output=lmdb_cv_rnn_%j.txt
 #SBATCH --partition=mcs.gpu.q         # Choose a partition that has GPUs
-#SBATCH --time=30:00:00
+#SBATCH --time=16:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=16G
+#SBATCH --mem-per-cpu=8G
 #SBATCH --gpus=1
 
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
