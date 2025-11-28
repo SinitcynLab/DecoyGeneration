5b300e11037c037625260e0019fb4814741b0ee7
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-11-29 00:04:14 +0100

    Adapted job to be run

--- src/bash/run_job.sh
+++ src/bash/run_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=rnn_d_e_all_data
-#SBATCH --output=rnn_d_e_all_data_%j.txt
+#SBATCH --output=rnn_e_all_data_%j.txt
 #SBATCH --partition=mcs.gpu.q         # Choose a partition that has GPUs
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
