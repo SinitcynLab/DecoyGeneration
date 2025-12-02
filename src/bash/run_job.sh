c9889cedd111222ef78d509fd5e0c26351e6a426
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-12-02 10:58:47 +0100

    Changed job and generator

--- src/bash/run_job.sh
+++ src/bash/run_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=rnn_s_all_data
-#SBATCH --output=rnn_s_all_data_%j.txt
-#SBATCH --partition=mcs.gpu.q         # Choose a partition that has GPUs
-#SBATCH --time=16:00:00
+#SBATCH --job-name=gen
+#SBATCH --output=gen_%j.txt
+#SBATCH --partition=tue.gpu.q         # Choose a partition that has GPUs
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
@@ -17,4 +17,4 @@ source activate decoy_gen
 
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 
-python -u src/run/cross_val_rnn.py
+python -u src/main.py
--- src/main.py
+++ src/main.py
@@ -26,15 +26,16 @@ if __name__ == "__main__":
     n: int = 1
     random: Random = Random(42)
     generators: List[DecoyGenerator] = [
-        ReverseGenerator(
-            special_amino_acids
-        ),
-        DiannGenerator(
-            special_amino_acids
-        ),
-        ShuffleGenerator(
-            special_amino_acids,
-            random=random
+        EsmGenerator(
+            local_path='models/esm2_t33_650M_UR50D',
+            random=random,
+            special_amino_acids=special_amino_acids,
+            sort_optimization=True,
+            batch_size=1,
+            ml_generator_type=MlGeneratorType.BEST,
+            device=device,
+            masking_type=MaskingType.COUNT,
+            mask_count=1
         )
     ]
     for generator in generators:
