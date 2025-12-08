23e28bfd9b64c78073da4f5823e28f2c5319a42e
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-12-08 11:01:21 +0100

    changed job

--- src/bash/run_job.sh
+++ src/bash/run_job.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --job-name=mass_smart_mask
 #SBATCH --output=gen_mass_%j.txt
-#SBATCH --partition=tue.gpu.q         # Choose a partition that has GPUs
-#SBATCH --time=16:00:00
+#SBATCH --partition=mcs.gpu.q         # Choose a partition that has GPUs
+#SBATCH --time=10:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
@@ -17,4 +17,4 @@ source activate decoy_gen
 
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 
-python -u src/main.py
+python -u src/run/cross_val_rnn.py
