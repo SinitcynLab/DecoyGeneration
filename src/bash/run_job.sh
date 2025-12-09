a0e6c725106904e0ddafa192ce75a8d646c82dce
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-12-09 10:38:25 +0100

    Update run_job.sh

--- src/bash/run_job.sh
+++ src/bash/run_job.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=rnn_ms_all_data
 #SBATCH --output=rnn_ms_all_data_%j.txt
-#SBATCH --partition=tue.cpu.q         # Choose a partition that has GPUs
+#SBATCH --partition=tue.default.q         # Choose a partition that has GPUs
 #SBATCH --time=1:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
