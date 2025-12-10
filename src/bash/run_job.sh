c2b0394bb877bb0b33bf1e64dd944b1d0dccc989
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-12-10 16:04:11 +0100

    Update run_job.sh

--- src/bash/run_job.sh
+++ src/bash/run_job.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-#SBATCH --job-name=timing
-#SBATCH --output=timing_%j.txt
-#SBATCH --partition=tue.default.q         # Choose a partition that has GPUs
+#SBATCH --job-name=gen_smart
+#SBATCH --output=gen_smart_%j.txt
+#SBATCH --partition=tue.gpu.q         # Choose a partition that has GPUs
 #SBATCH --time=16:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=16G
+#SBATCH --gpus=1
 
 module load Python/3.11.3-GCCcore-12.3.0 
 module load Anaconda3/2023.09-0
@@ -16,4 +17,4 @@ source activate decoy_gen
 
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 
-python -u src/run/timing_test.py
+python -u src/main.py
