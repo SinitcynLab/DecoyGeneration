175e39ee14c3c7aabf71be51359e2fb0877db13d
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-12-18 00:20:18 +0100

    update job

--- src/bash/run_job.sh
+++ src/bash/run_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=gen_mass_smart_%j
-#SBATCH --output=gen_mass_smart_(17_12)_%j.txt
-#SBATCH --partition=tue.gpu.q         # Choose a partition that has GPUs
-#SBATCH --time=16:00:00
+#SBATCH --job-name=get_distr_650_%j
+#SBATCH --output=get_distr_650_%j.txt
+#SBATCH --partition=mcs.gpu.q         # Choose a partition that has GPUs
+#SBATCH --time=3:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
--- src/main.py
+++ src/main.py
@@ -24,11 +24,12 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
 
+    N = 500
     n: int = 1
     random: Random = Random(42)
     generators: List[DecoyGenerator] = [
         MaxProbMaskingEsmGenerator(
-            local_path="models/esm2_t6_8M_UR50D",
+            local_path="models/esm2_t33_650M_UR50D",
             random=random,
             special_amino_acids=special_amino_acids,
             sort_optimization=True,
@@ -41,10 +42,10 @@ if __name__ == "__main__":
         filename, extension = os.path.splitext(target_filename)
         if issubclass(type(generator), MlGenerator):
             for i in range(n):
-                filename_out = f"{target_filename}.{generator}.{i}{extension}"
+                filename_out = f"{filename}.{generator}.{i}{extension}"
                 target_records = [record for record in read_fasta_file(target_filename)]
                 target_records = remove_long_sequences(target_records, cap_length=10_000)
-                batch_starts = np.arange(0, len(target_records), generator.batch_size)
+                batch_starts = np.arange(0, N, generator.batch_size)
                 for start in batch_starts:
                     end = min(start + generator.batch_size, len(target_records))
                     write_fasta_file(filename_out, generator.convert_fasta(target_records[start:end]), 60, 'a')
