833813fd74d04ff907558be9745a10f1b76f88b9
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-12-18 10:43:42 +0100

    capped seq length to 3000 for 3B

--- src/main.py
+++ src/main.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
             for i in range(n):
                 filename_out = f"{target_filename}.{generator}.{i}{extension}"
                 target_records = [record for record in read_fasta_file(target_filename)]
-                target_records = remove_long_sequences(target_records, cap_length=10_000)
+                target_records = remove_long_sequences(target_records, cap_length=3000)
                 batch_starts = np.arange(0, len(target_records), generator.batch_size)
                 for start in batch_starts:
                     end = min(start + generator.batch_size, len(target_records))
