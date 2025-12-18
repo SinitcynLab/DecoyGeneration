b0a71ee26e8ea83db49aceeab3dd17299e1aa69b
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-12-18 22:35:40 +0100

    Update shorten.py

--- src/shorten.py
+++ src/shorten.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     fasta_records = read_fasta_file(target_filename)
     fasta_records = [record for record in fasta_records]
     fasta_records = fasta_records[0:3008]
-    write_fasta_file("data/decoys/UP000002311_559292.mass_smart_masking_esm_8M.0.fasta")
+    write_fasta_file("data/decoys/UP000002311_559292.mass_smart_masking_esm_8M.0.fasta", fasta_records)
