598402d1605139b1e60d49d6b0e22a5362d763dd
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-12-18 10:39:35 +0100

    update job

--- src/main.py
+++ src/main.py
@@ -27,14 +27,16 @@ if __name__ == "__main__":
     n: int = 1
     random: Random = Random(42)
     generators: List[DecoyGenerator] = [
-        MaxProbMaskingEsmGenerator(
-            local_path="models/esm2_t6_8M_UR50D",
+        EsmGenerator(
+            local_path="models/esm2_t36_3B_UR50D",
             random=random,
             special_amino_acids=special_amino_acids,
             sort_optimization=True,
             batch_size=1,
             ml_generator_type=MlGeneratorType.BEST,
-            device=device
+            device=device,
+            masking_type=MaskingType.COUNT,
+            mask_count=1
         )
     ]
     for generator in generators:
