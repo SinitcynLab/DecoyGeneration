35a90769cda1b79fb82240eaad56a02c27eb291b
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-12-18 13:26:08 +0100

    Update run_job.sh

--- src/bash/run_job.sh
+++ src/bash/run_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=get_distr_%j
-#SBATCH --output=get_distr_%j.txt
+#SBATCH --job-name=get_distr_complete_%j
+#SBATCH --output=get_distr_complete_%j.txt
 #SBATCH --partition=mcs.gpu.q         # Choose a partition that has GPUs
 #SBATCH --time=10:00:00
 #SBATCH --nodes=1
