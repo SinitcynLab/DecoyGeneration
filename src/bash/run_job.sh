01c9a7c49f6c16731ec72e8450655136680d1dda
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2025-12-18 13:26:58 +0100

    Update run_job.sh

--- src/bash/run_job.sh
+++ src/bash/run_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=get_distr_complete_%j
 #SBATCH --output=get_distr_complete_%j.txt
 #SBATCH --partition=mcs.gpu.q         # Choose a partition that has GPUs
-#SBATCH --time=10:00:00
+#SBATCH --time=5:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
