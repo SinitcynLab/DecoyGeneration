e4ca146217421f13e0f776fc3e21ed9ea6b7a949
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2026-01-01 02:49:19 +0100

    update job

--- src/bash/run_job.sh
+++ src/bash/run_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=all_data_new_param_counts_%j
 #SBATCH --output=all_data_new_param_counts_%j.txt
 #SBATCH --partition=mcs.gpu.q      # Choose a partition that has GPUs
-#SBATCH --time=12:00:00
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
