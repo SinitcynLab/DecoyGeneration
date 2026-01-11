0b8e770a79f0d7b6bb425e56efae2ce8895c6f83
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2026-01-11 20:01:12 +0100

    changed job

--- src/bash/run_job.sh
+++ src/bash/run_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=cnn_spectra_shuffle_esm650_%j
-#SBATCH --output=cnn_spectra_shuffle_esm650_%j.txt
+#SBATCH --job-name=rnn_spectra_shuffle_esm650_%j
+#SBATCH --output=rnn_spectra_shuffle_esm650_%j.txt
 #SBATCH --partition=tue.gpu.q      # Choose a partition that has GPUs
 #SBATCH --time=12:00:00
 #SBATCH --nodes=1
@@ -17,4 +17,4 @@ source activate decoy_gen
 
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 
-python -u src/run/cross_val_mlp_spectra.py
+python -u src/run/cross_val_rnn_spectra.py
--- src/run/cross_val_rnn_spectra.py
+++ src/run/cross_val_rnn_spectra.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     # target data:
     target_records = read_fasta_file(target_file)
     target_sequences = [record.sequence for record in target_records]
-    N = 100
+    N = len(target_sequences)
     target_lmdb_path = f"{temp_encoding_dir}/targets.lmdb"
     encode_seqs_to_lmdb(target_sequences[0:N], encoder, target_lmdb_path)
 
@@ -56,7 +56,7 @@ if __name__ == "__main__":
             decoy_records = read_fasta_file(decoy_file)
             decoy_sequences = [record.sequence for record in decoy_records]
             decoy_lmdb_path = f"{temp_encoding_dir}/{decoy_ids[i]}.lmdb"
-            M = 100#len(decoy_sequences)
+            M = len(decoy_sequences)
             encode_seqs_to_lmdb(decoy_sequences[0:M], encoder, decoy_lmdb_path)
             labels = torch.cat((torch.zeros(N), torch.ones(M)))
             dataset = LMDBDataset([target_lmdb_path, decoy_lmdb_path], labels)
