298e6ccb03f241adabc14d08ab494d05bf407036
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2026-01-21 12:12:40 +0100

    modified umap kernel

--- src/run/cross_val_svm_umap_kernel.py
+++ src/run/cross_val_svm_umap_kernel.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     # target data:
     target_records = read_fasta_file(target_file)
     target_sequences = [record.sequence for record in target_records]
-    N = 15000#len(target_sequences)
+    N = len(target_sequences)
     target_lmdb_path = f"{temp_encoding_dir}/targets.lmdb"
     encode_seqs_to_lmdb(target_sequences[0:N], encoder, target_lmdb_path, 512)
 
@@ -44,7 +44,7 @@ if __name__ == "__main__":
         else:
             decoy_records = read_fasta_file(decoy_file)
             decoy_sequences = [record.sequence for record in decoy_records]
-            M = 15000#len(decoy_sequences)
+            M = len(decoy_sequences)
             decoy_lmdb_path = f"{temp_encoding_dir}/{decoy_ids[i]}.lmdb"
             encode_seqs_to_lmdb(decoy_sequences[0:M], encoder, decoy_lmdb_path, 512)
             labels = torch.cat((torch.zeros(N), torch.ones(M)))
