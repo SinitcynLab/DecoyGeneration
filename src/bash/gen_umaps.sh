357d6f7a8f8aec5423104a27743e1a95fbc8505c
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2026-01-22 11:27:10 +0100

    created umap visualizer bash script

---
+++ src/bash/gen_umaps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --job-name=target_test_mlp_%j
+#SBATCH --output=target_test_mlp_%j.txt
+#SBATCH --partition=mcs.gpu.q      # Choose a partition that has GPUs
+#SBATCH --time=10:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=16G
+#SBATCH --gpus=1 
+
+module load Python/3.11.3-GCCcore-12.3.0
+module load Anaconda3/2023.09-0
+eval "$(conda shell.bash activate)"
+source activate decoy_gen
+
+module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
+
+python -u src/visualization/umap_visualizer.py --files data/targets/UP000002311_559292.fasta data/decoys/UP000002311_559292.shuffle.0.fasta data/decoys/UP000002311_559292.esm650M.best.c1.0.fasta --identifiers "Target" "Shuffle" "ESM 650" --number 6059
+python -u src/visualization/umap_visualizer.py --files data/targets/UP000002311_559292.fasta data/decoys/UP000002311_559292.reverse.fasta data/decoys/UP000002311_559292.esm650M.best.c1.0.fasta --identifiers "Target" "Reverse" "ESM 650" --number 6059
+python -u src/visualization/umap_visualizer.py --files data/targets/UP000002311_559292.fasta data/decoys/UP000002311_559292.diann.fasta data/decoys/UP000002311_559292.esm650M.best.c1.0.fasta --identifiers "Target" "DIA-NN" "ESM 650" --number 6059
--- src/visualization/umap_visualizer.py
+++ src/visualization/umap_visualizer.py
@@ -2,10 +2,12 @@ import torch
 import numpy as np
 import umap
 import pandas as pd
+import argparse
 
 from umap import plot
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
+from typing import Iterable
 
 from src.peptide_classifiers.nn_classifier import cross_validate_nn
 from src.peptide_classifiers.feed_forward_nn_classifier import FeedForwardNNClassifier
@@ -15,46 +17,16 @@ from src.io.fasta import read_fasta_file
 from src.io.lmdb_writer import encode_seqs_to_lmdb, delete_lmdb
 from src.io.lmdb_dataset import LMDBDataset
 
-def generate_umap_image(targets: LMDBDataset, decoys: LMDBDataset, path: str, decoy_type: str):
-    target_data, _ = targets.get_pairs(range(targets.size()))
-    decoy_data, _ = decoys.get_pairs(range(decoys.size()))
-    max_dim: int = max([torch.numel(t) for t in target_data] + [torch.numel(t) for t in decoy_data])
-    tot_size = targets.size() + decoys.size()
-    
-    plot_data: np.ndarray = np.zeros((tot_size, max_dim))
-    plot_labels: np.ndarray = np.zeros(tot_size)
-    for i in range(len(target_data)):
-        plot_data[i,0:torch.numel(target_data[i])] = target_data[i].cpu().flatten().numpy()
-        plot_labels[i] = 0
-    for i in range(len(decoy_data)):
-        plot_data[i,0:torch.numel(target_data[i])] = target_data[i].cpu().flatten().numpy()
-        plot_labels[i] = 1
-
-    mapper = umap.UMAP().fit(plot_data)
-    plot.points(mapper, labels=plot_labels)
-    plt.legend(('Targets', f'Decoys ({decoy_type})'))
-    plt.title(f"UMAP visualization of target vs decoy sequences ({decoy_type})")
-    plt.savefig(path)
-
-if __name__ == "__main__":
+def generate_umap_image(files: Iterable[str], seq_ids: Iterable[str], number: int):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(device)
-    print(torch.get_num_threads())
     encoder = ProtBertClsEncoder(device)
-
-    # Choose base file:
-    base = 'UP000002311_559292'
-    files = [f'data/targets/{base}.fasta', f'data/decoys/{base}.shuffle.0.fasta', f'data/decoys/{base}.esm650M.best.c1.0.fasta']
-    seq_ids = ('Target', 'Shuffle', 'ESM 650M')
-    
     print("Generating UMAP image...")
     data_list = []
     label_list = []
     for i, file in enumerate(files):
         records = read_fasta_file(file)
         sequences = [record.sequence for record in records]
-        N = 200#len(sequences)
-        encodings = encoder(sequences[0:N])
+        encodings = encoder(sequences[0:number])
         file_data: np.ndarray = encodings.numpy()
         file_labels = np.ones(N, dtype=int) * i
         
@@ -69,13 +41,24 @@ if __name__ == "__main__":
     size_val = 1
     cmap = plt.get_cmap("Set1")
 
-    for label in np.unique(labels):
-        indices_with_label = np.where(labels == label)
+    for file_suffix in np.unique(labels):
+        indices_with_label = np.where(labels == file_suffix)
         data_to_plot = embedding[indices_with_label]
-        plt.scatter(data_to_plot[:,0], data_to_plot[:,1], s=size_val, c=cmap(label))
+        plt.scatter(data_to_plot[:,0], data_to_plot[:,1], s=size_val, c=cmap(file_suffix))
 
     plt.gca().xaxis.set_major_formatter(FormatStrFormatter('%d'))
     plt.gca().legend(seq_ids, markerscale=5//size_val)
     plt.subplots_adjust(top=0.83)
     plt.title(f"UMAP visualization of target sequences, sequences generated \n using the shuffle procedure and sequences \n generator using ESM 650M")
-    plt.savefig("src/visualization/images/umap/umap_publish_target_v_shuffle.png")
+    file_suffix = "_".join(seq_ids)
+    plt.savefig(f"src/visualization/images/umap/umap_{file_suffix}.png")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--files", help="What files to obtain sequences from to plot in a UMAP image.")
+    parser.add_argument("-i", "--identifiers", help="What labels to use for the sequences from the various files.")
+    parser.add_argument("-n", "--number", "The number of points to plot per class in the UMAP image.")
+
+    args = parser.parse_args()
+
+    generate_umap_image(args.files, args.identifiers, args.number)
