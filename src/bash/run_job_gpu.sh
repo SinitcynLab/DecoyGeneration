9870893bd28226010c200b827dc257ef0fc29b5e
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2026-01-28 10:27:47 +0100

    changed job

--- src/bash/run_job_gpu.sh
+++ src/bash/run_job_gpu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=mlp_all_data_bucket_%j
-#SBATCH --output=mlp_all_data_bucket_%j.txt
-#SBATCH --partition=tue.gpu.q      # Choose a partition that has GPUs
-#SBATCH --time=12:00:00
+#SBATCH --job-name=rnn_all_data_bucket_%j
+#SBATCH --output=rnn_all_data_bucket_%j.txt
+#SBATCH --partition=mcs.gpu.q      # Choose a partition that has GPUs
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
@@ -17,4 +17,4 @@ source activate decoy_gen
 
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 
-python -u src/run/cross_val_mlp.py
+python -u src/run/cross_val_rnn.py
