284bb2aae26e6365450e8712656bc8c4c521707c
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2026-01-29 17:01:13 +0100

    Update run_job_gpu.sh

--- src/bash/run_job_gpu.sh
+++ src/bash/run_job_gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=gen_650M_new_%j
-#SBATCH --output=gen_650M_new_%j.txt
+#SBATCH --job-name=rnn_all_data_650M_new_%j
+#SBATCH --output=rnn_all_data_650M_new_%j.txt
 #SBATCH --partition=mcs.gpu.q      # Choose a partition that has GPUs
 #SBATCH --time=10:00:00
 #SBATCH --nodes=1
