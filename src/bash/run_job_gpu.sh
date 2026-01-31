56230c39f07da64d9284be65464260f453b9efb5
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2026-01-31 23:53:37 +0100

    changed job

--- src/bash/run_job_gpu.sh
+++ src/bash/run_job_gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=mlp_all_data_max_prob_new_%j
-#SBATCH --output=mlp_all_data_max_prob_new_%j.txt
+#SBATCH --job-name=gen_crap_%j
+#SBATCH --output=gen_crap_%j.txt
 #SBATCH --partition=mcs.gpu.q      # Choose a partition that has GPUs
 #SBATCH --time=4:00:00
 #SBATCH --nodes=1
@@ -17,4 +17,4 @@ source activate decoy_gen
 
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 
-python -u src/decoy_gen.py --command evaluate --classifier mlp --target_file data/targets/UP000002311_559292.fasta --decoy_files data/decoys/UP000002311_559292.max_prob_smart_masking_esm_8M.0.fasta --decoy_ids random_replace
+python -u src/decoy_gen.py --command generate --target_file data/targets/crap.fasta --output_directory data/decoys --generators smart_masking_esm
