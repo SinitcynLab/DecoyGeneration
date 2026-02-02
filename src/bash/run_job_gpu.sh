4b05a5f02b6e8de85f816169a1a33f3195d8d570
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2026-02-02 21:39:37 +0100

    Update run_job_gpu.sh

--- src/bash/run_job_gpu.sh
+++ src/bash/run_job_gpu.sh
@@ -17,4 +17,4 @@ source activate decoy_gen
 
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 
-python -u src/decoy_gen.py --command generate --generators esm_c_terminus esm_n_terminus --target_file data/targets/UP000002311_559292.fasta --output_directory data/decoys
+python -u src/decoy_gen.py --command evaluate --classifier mlp --target_file data/targets/UP000002311_559292.fasta --decoy_files data/decoysUP000002311_559292.C_terminus_esm650M.best.c1.0.fasta data/decoysUP000002311_559292.N_terminus_esm650M.best.c1.0.fasta --decoy_ids esm_c_terminus esm_n_terminus
