4d47e0eed9370eb9bd3d79ab9ee617f3faaed385
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2026-02-02 18:05:40 +0100

    changed job

--- src/bash/run_job_gpu.sh
+++ src/bash/run_job_gpu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=mlp_random_smart_gen_%j
-#SBATCH --output=mlp_random_smart_gen_%j.txt
+#SBATCH --job-name=random_smart_gen_%j
+#SBATCH --output=random_smart_gen_%j.txt
 #SBATCH --partition=mcs.gpu.q      # Choose a partition that has GPUs
-#SBATCH --time=1:00:00
+#SBATCH --time=8:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
@@ -17,4 +17,4 @@ source activate decoy_gen
 
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 
-python -u src/decoy_gen.py --command evaluate --classifier mlp --target_file data/targets/UP000002311_559292.fasta --decoy_files data/decoys/UP000002311_559292.random_pos_smart_masking_esm_8M.0.fasta --decoy_ids random_smart
+python -u src/decoy_gen.py --command generate --generators esm_c_terminus esm_n_terminus --target_file data/targets/UP000002311_559292.fasta --output_directory data/decoys
