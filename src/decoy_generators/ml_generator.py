73f6c7e940844989d533c2f3c8939e4ff9668f52
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2026-02-06 12:48:09 +0100

    added more logging

--- src/decoy_generators/ml_generator.py
+++ src/decoy_generators/ml_generator.py
@@ -152,12 +152,18 @@ class MlGenerator(DecoyGenerator):
                 
             yield "".join(new_sequence)
 
-    def _log_data(self, probs: Tensor, sequence_idx: int, mask_position: int, og_aa: str, chosen_aa: str, top_aa: str):
-        relevant_aa_ids = self.tokenizer.convert_tokens_to_ids([og_aa, chosen_aa, top_aa])
+    def _log_data(self, probs: Tensor, sequence_idx: int, mask_position: int, sequence: str, chosen_aa: str, top_aa: str):
+        aa_i = sequence[mask_position]
+        aa_i_min_1 = sequence[mask_position - 1]
+        aa_i_plus_1 = sequence[mask_position + 1]
+        relevant_aa_ids = self.tokenizer.convert_tokens_to_ids([aa_i, chosen_aa, top_aa])
         relevant_aa_probs = probs[sequence_idx, mask_position, relevant_aa_ids] # [prob_og_aa, prob_chosen_aa, prob_top_aa]
         # log the original token:
         with open(f'og_aa_{self}.txt', 'a') as file:
-            file.write(f"{og_aa}\n")
+            file.write(f"{aa_i}\n")
+        # log the offset tokens:
+        with open(f'aa_min_offset_{self}.txt', 'a') as file:
+            file.write(f"{aa_i_min_1},{aa_i_plus_1}")
         # log the most-probable token:
         with open(f'most_probable_aa_{self}.txt', 'a') as file:
             file.write(f"{top_aa}\n")
