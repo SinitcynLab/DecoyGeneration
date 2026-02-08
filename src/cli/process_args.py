aa8dae998d68820d26908d2b2fd743cdaaf47d43
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2026-02-08 22:35:00 +0100

    implemented non-masking smart generator

--- src/cli/process_args.py
+++ src/cli/process_args.py
@@ -10,7 +10,7 @@ from src.decoy_generators.esm_generator import EsmGenerator, MaskingType, MlGene
 from src.decoy_generators.terminus_esm_generator import TerminusEsmGenerator
 from src.decoy_generators.reverse_generator import ReverseGenerator
 from src.decoy_generators.shuffle_generator import ShuffleGenerator
-from src.decoy_generators.smart_masking_esm import MaxProbMaskingEsmGenerator, RelDiffMaskingEsmGenerator, MaxEntropyEsmGenerator
+from src.decoy_generators.smart_masking_esm import MaxProbMaskingEsmGenerator, MaxEntropyEsmGenerator
 from src.decoy_generators.random_replace_generator import RandomReplaceGenerator
 from src.run.cross_val_mlp import cross_val_mlp
 from src.run.cross_val_rnn import cross_val_rnn
@@ -120,17 +120,7 @@ def create_generator_from_string(generator_string: str, seed: int, mask_count: i
         )
     elif generator_string == "max_prob_smart_esm":
         generator = MaxProbMaskingEsmGenerator(
-            local_path="models/esm2_t6_8M_UR50D",
-            random=random,
-            special_amino_acids=special_amino_acids,
-            sort_optimization=True,
-            batch_size=1,
-            ml_generator_type=MlGeneratorType.BEST,
-            device=device,
-        )
-    elif generator_string == "rel_diff_smart_esm":
-        generator = RelDiffMaskingEsmGenerator(
-            local_path="models/esm2_t6_8M_UR50D",
+            local_path="models/esm2_t33_650M_UR50D",
             random=random,
             special_amino_acids=special_amino_acids,
             sort_optimization=True,
@@ -166,7 +156,7 @@ def create_generator_from_string(generator_string: str, seed: int, mask_count: i
         )
     elif generator_string == "max_entropy_esm":
         generator = MaxEntropyEsmGenerator(
-            local_path="models/esm2_t6_8M_UR50D",
+            local_path="models/esm2_t33_650M_UR50D",
             random=random,
             special_amino_acids=special_amino_acids,
             sort_optimization=True,
--- src/decoy_generators/smart_masking_esm.py
+++ src/decoy_generators/smart_masking_esm.py
@@ -6,49 +6,27 @@ from src.decoy_generators.base_smart_masking_esm import BaseSmartMaskingEsmGener
 from torch import Tensor
 
 class MaxProbMaskingEsmGenerator(BaseSmartMaskingEsmGenerator):
-    def _get_score_and_token_choice(self, probs: Tensor, pos: int, original_aa: str):
+    def _get_score(self, probs: Tensor, pos: int, original_aa: str):
         # find the top probability of aa's:
         token_prob, amino_acid_indices = torch.topk(probs[0, pos, self.aa_ids], k=self.k, largest=True)
         # convert list of tokens (indices of self.canonical_amino_acids) to a list of strings:
         amino_acids = [self.canonical_amino_acids[index] for index in amino_acid_indices]
-        # the token probability is the score in this case
-        score, aa_choice = self._get_feasible_token_with_max_score(original_aa, token_prob, amino_acids)
-        # return feasible token with highest score and corresponding score:
-        return score, aa_choice
+        # the token probability is the score in this case:
+        score = self._get_highest_feasible_score(original_aa, token_prob, amino_acids)
+        # return highest feasible score:
+        return score
     
     def __str__(self):
         return f"max_prob_{super().__str__()}"
     
-class MaxEntropyEsmGenerator(MaxProbMaskingEsmGenerator):
-    def _get_score_and_token_choice(self, probs, pos, original_aa):
-        # get the aa_choice as the feasible token with max probability (just like in default ESM):
-        _, aa_choice = MaxProbMaskingEsmGenerator._get_score_and_token_choice(self, probs, pos, original_aa)
+class MaxEntropyEsmGenerator(BaseSmartMaskingEsmGenerator):
+    def _get_score(self, probs, pos, original_aa):
         # get the distribution over feasible tokens:
         token_prob, _ = torch.topk(probs[0, pos, self.aa_ids], k=self.k, largest=True)
         # compute entropy of distribution (this scipy function normalizes by default):
         score = entropy(token_prob.cpu().numpy())
-        # return feasible token with largest probability and entropy as score:
-        return score, aa_choice
+        # return entropy as score
+        return score
     
     def __str__(self):
         return f"max_entropy_{super().__str__()}"
-    
-class RelDiffMaskingEsmGenerator(BaseSmartMaskingEsmGenerator):
-    def _get_score_and_token_choice(self, probs: Tensor, position: int, original_aa: str):
-        # get the original aa's prob:
-        # find the id of the originally present aa, and find the probability corresponding to this aa as judged by ESM:
-        og_aa_id = self.tokenizer.convert_tokens_to_ids(original_aa) # note that this id is not the same as the index of the aa in 'self.canonical_amino_acids'
-        og_prob = probs[0, position, og_aa_id]
-        
-        tokens_prob, amino_acid_indices = torch.topk(probs[0, position, self.aa_ids], k=self.k, largest=True)
-        amino_acids = [self.canonical_amino_acids[index] for index in amino_acid_indices]
-        
-        scores = tokens_prob.clone()
-        for i, _ in enumerate(tokens_prob):
-            scores[i] = - ((og_prob - tokens_prob[i])) / og_prob # multiply by -1 because find the max score
-        
-        score, aa_choice = self._get_feasible_token_with_max_score(original_aa, scores, amino_acids)
-        return score, aa_choice
-    
-    def __str__(self):
-        return f"rel_diff_{super().__str__()}"
