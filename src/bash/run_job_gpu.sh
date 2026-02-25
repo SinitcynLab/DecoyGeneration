24a51727240c9f2819328cdae06b434397875960
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2026-02-25 11:04:36 +0100

    Update run_job_gpu.sh

--- src/bash/run_job_gpu.sh
+++ src/bash/run_job_gpu.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=mlp_esm_NC_%j
 #SBATCH --output=mlp_esm_NC_%j.txt
 #SBATCH --partition=mcs.gpu.q      # Choose a partition that has GPUs
-#SBATCH --time=24:00:00
+#SBATCH --time=3:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
@@ -17,4 +17,4 @@ source activate decoy_gen
 
 module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
 
-python src/decoy_gen.py --command evaluate --classifier mlp --target_file data/targets/UP000002311_559292.fasta --decoy_files data/decoys/UP000002311_559292.NC_terminus_facebook_esm2_t33_650M_UR50D.best.c1.f32.0.fasta --decoy_ids esm_NC
+python -u src/decoy_gen.py --command evaluate --classifier mlp --target_file data/targets/UP000002311_559292.fasta --decoy_files data/decoys/UP000002311_559292.NC_terminus_facebook_esm2_t33_650M_UR50D.best.c1.f32.0.fasta --decoy_ids esm_NC
