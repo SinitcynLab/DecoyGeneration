fc9b4c2a3c37b17da56ba534769961b717caf2de
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2026-02-25 11:25:29 +0100

    Update run_job_cpu.sh

--- src/bash/run_job_cpu.sh
+++ src/bash/run_job_cpu.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=svm_umap_%j
 #SBATCH --output=svm_umap_%j.txt
 #SBATCH --partition=tue.default.q      # Choose a partition that has GPUs
-#SBATCH --time=12:00:00
+#SBATCH --time=1:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=6
