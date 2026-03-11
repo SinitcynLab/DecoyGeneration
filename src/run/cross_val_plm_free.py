def5eff9cf9719a89adfa1145bc0918aa38704e1
Fabrice-Kusters <121855436+FabriceTUe@users.noreply.github.com>
2026-03-11 14:45:44 +0100

    minor bug fix

--- src/run/cross_val_plm_free.py
+++ src/run/cross_val_plm_free.py
@@ -55,7 +55,7 @@ def cross_val_plm_free(target_file: str, decoy_files: Iterable[str], decoy_ids: 
     target_records = read_fasta_file(target_file)
     target_sequences = [record.sequence for record in target_records]
     target_lmdb_path = f"{temp_encoding_dir}/targets.lmdb"
-    N = len(target_sequences)
+    N = get_peptide_number(target_sequences, protease)
     encode_seqs_to_lmdb(target_sequences, encoder, target_lmdb_path)
 
     print(f"Cross validation of the PLM-free classifier (peptide_level={encoder.peptide_level}):")
@@ -67,7 +67,7 @@ def cross_val_plm_free(target_file: str, decoy_files: Iterable[str], decoy_ids: 
             decoy_records = read_fasta_file(decoy_file)
             decoy_sequences = [record.sequence for record in decoy_records]
             decoy_lmdb_path = f"{temp_encoding_dir}/{decoy_ids[i]}.lmdb"
-            M = len(decoy_sequences)
+            M = get_peptide_number(decoy_sequences, protease)
             encode_seqs_to_lmdb(decoy_sequences, encoder, decoy_lmdb_path)
             labels = torch.cat((torch.zeros(N), torch.ones(M)))
             dataset = LMDBDataset([target_lmdb_path, decoy_lmdb_path], labels)
